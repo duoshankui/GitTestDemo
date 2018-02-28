f20d208d7f78918561c3eb66d4fe7adfad2e58e0
DoubleK <2578138035@qq.com>
2018-02-28 16:55:41 +0800

    新增文件类

---
+++ GitTestDemo/NewAddAnotherClass.h
@@ -0,0 +1,13 @@
+//
+//  NewAddAnotherClass.h
+//  GitTestDemo
+//
+//  Created by DoubleK on 2018/2/28.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewAddAnotherClass : NSObject
+
+@end
---
+++ GitTestDemo/NewAddAnotherClass.m
@@ -0,0 +1,13 @@
+//
+//  NewAddAnotherClass.m
+//  GitTestDemo
+//
+//  Created by DoubleK on 2018/2/28.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import "NewAddAnotherClass.h"
+
+@implementation NewAddAnotherClass
+
+@end
