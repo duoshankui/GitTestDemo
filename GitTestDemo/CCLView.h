6d522a109b236d38be1747af9e38459015d9282b
CCL <ccl@CCLdeMacBook-Pro.local>
2018-03-01 13:46:59 +0800

    CCL新增文件

---
+++ GitTestDemo/CCLView.h
@@ -0,0 +1,13 @@
+//
+//  CCLView.h
+//  GitTestDemo
+//
+//  Created by CCL on 2018/3/1.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCLView : UIView
+
+@end
---
+++ GitTestDemo/CCLView.m
@@ -0,0 +1,21 @@
+//
+//  CCLView.m
+//  GitTestDemo
+//
+//  Created by CCL on 2018/3/1.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import "CCLView.h"
+
+@implementation CCLView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
