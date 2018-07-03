75cce0dabffbdf73993884578d4c2853b316c7b9
duoshankui <2578138035@qq.com>
2018-07-03 11:49:19 +0800

    新增gitBtn2

--- GitTestDemo/ViewController.m
+++ GitTestDemo/ViewController.m
@@ -22,6 +22,9 @@
     UIButton *gitBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
     [gitBtn setBackgroundColor:[UIColor redColor]];
     [self.view addSubview:gitBtn];
+    [gitBtn2 setBackgroundColor:[UIColor blueColor]];
+    [self.view addSubview:gitBtn2];
+
 }
 
 
